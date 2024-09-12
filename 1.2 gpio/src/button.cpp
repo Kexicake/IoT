4086cc909cdfdeff4c536a93abb69b3785e41c78
Kexicake <slava.kuznetsov.2017@gmail.com>
2024-09-12 14:38:23 +0400

    Попытка вывести обработку кнопки в библиотеку

---
+++ 1.2 gpio/src/button.cpp
@@ -0,0 +1,45 @@
+#include "mbed.h"
+#include "button.h"
+
+Button::Button(PinName PinBut) : myButton(PinBut){
+    myButton.mode(PullUp);
+    timer.start();
+}
+bool Button::press(){
+    if (myButton && !flag && timer.read_ms() - btnTimer > 100) {
+        flag = true;
+        btnTimer = timer.read_ms();
+        printf("Button state is: press\n");
+        return 1;
+    }
+    return 0;
+}
+bool Button::hold(){
+    if (myButton && flag && timer.read_ms() - btnTimer > 500) {
+        flag = true;
+        btnTimer = timer.read_ms();
+        printf("Button state is: hoooold\n");
+        return 1;
+    }
+    return 0;
+}
+bool Button::relize(){
+    if (myButton && flag && timer.read_ms() - btnTimer > 100) {
+        flag = false;
+        btnTimer = timer.read_ms();
+        printf("Button state is: relise\n");
+        return 1;
+    } 
+    return 0;
+}
+
+int Button::status(){
+    if (press()){
+        return 1;
+    }else if(hold()){
+        return 2;
+    }else if(relize()){
+        return 0;
+    }
+    return 0;
+}
--- 1.2 gpio/src/main.cpp
+++ 1.2 gpio/src/main.cpp
@@ -1,33 +1,14 @@
 
 #include "mbed.h"
-
-DigitalIn mybutton(D4);
+#include "button.h"
+#include "iostream"
+Button mybutton(D4);
 DigitalOut myrele(D6);
-Timer timer, start;
-bool flag = false;
-long long btnTimer = 0;
 int main()
 {
-    mybutton.mode(PullUp);
-    timer.start();
-    while(1) {
-        if (mybutton && !flag && timer.read_ms() - btnTimer > 100) {
-            flag = true;
-            btnTimer = timer.read_ms();
-            myrele = 1;
-            printf("Button state is: press\n");
-        }
-        if (mybutton && flag && timer.read_ms() - btnTimer > 500) {
-            flag = true;
-            btnTimer = timer.read_ms();
-            myrele = 1;
-            printf("Button state is: hoooold\n");
-        }
-        if (!mybutton && flag && timer.read_ms() - btnTimer > 100) {
-            flag = false;
-            myrele = 0;
-            btnTimer = timer.read_ms();
-            printf("Button state is: relise\n");
-        } 
+    while (1)
+    {
+        mybutton.status();
     }
+    
 }
