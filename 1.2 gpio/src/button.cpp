d1fc5d5b152db85665d602c81148d34553d23bab
Kexicake <slava.kuznetsov.2017@gmail.com>
2024-09-20 09:58:37 +0400

    Исправлен баг с кнопкой

--- 1.2 gpio/src/button.cpp
+++ 1.2 gpio/src/button.cpp
@@ -9,7 +9,7 @@ bool Button::press(){
     if (myButton && !flag && timer.read_ms() - btnTimer > 100) {
         flag = true;
         btnTimer = timer.read_ms();
-        printf("Button state is: press\n");
+        //printf("Button state is: press\n");
         return 1;
     }
     return 0;
@@ -18,16 +18,16 @@ bool Button::hold(){
     if (myButton && flag && timer.read_ms() - btnTimer > 500) {
         flag = true;
         btnTimer = timer.read_ms();
-        printf("Button state is: hoooold\n");
+        //printf("Button state is: hoooold\n");
         return 1;
     }
     return 0;
 }
 bool Button::relize(){
-    if (myButton && flag && timer.read_ms() - btnTimer > 100) {
+    if (!myButton && flag && timer.read_ms() - btnTimer > 100) {
         flag = false;
         btnTimer = timer.read_ms();
-        printf("Button state is: relise\n");
+        //printf("Button state is: relise\n");
         return 1;
     } 
     return 0;
